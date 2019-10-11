bf1994d89d23ac4b5ea2d8ef7e7d0da0a76f5c98
Carl-Gerhard Lindesvärd <c.lindesvard@klarna.com>
2019-10-11 11:29:52 +0200

    init

---
+++ ios/Pods/Target Support Files/Pods-Storybook/Pods-Storybook-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Storybook : NSObject
+@end
+@implementation PodsDummy_Pods_Storybook
+@end
---
+++ storybook/webpack.config.js
@@ -0,0 +1,2 @@
+module.exports = async ({ config }) =>
+  console.dir(config, { depth: null }) || config;
